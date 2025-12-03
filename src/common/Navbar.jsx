71f222640928b4287fcb21658182e28899b6abd9
Vinay730 <vinay.juris@gmail.com>
2025-12-03 11:00:08 +0530

    install and setup

---
+++ src/common/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+       <h1 class="text-3xl font-bold underline text-center">
+    Navbar
+  </h1> 
+    </div>
+  )
+}
+
+export default Navbar
